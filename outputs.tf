0fb687c4dc9afa9cd6571d3fc53e2d84620417b3
Kim Iversen <kim@wwan.no>
2024-05-08 21:26:28 +0200

    fix: UCP resource id

--- outputs.tf
+++ outputs.tf
@@ -10,7 +10,7 @@ output "result" {
 
     // UCP resource IDs
     resources = [
-      "/planes/azure/azurecloud${azurerm_resource_group.this.id}"
+      "/planes/azure/azurecloud${azurerm_resource_group.this.id}/providers/Microsoft.Resources/resourceGroups/${azurerm_resource_group.this.name}"
     ]
   }
   description = "The result of the Recipe. Must match the target resource's schema."
