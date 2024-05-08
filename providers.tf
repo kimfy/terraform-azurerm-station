7c7b24d7312006db9943e9cda49b1696cea2e185
Kim Iversen <kim@wwan.no>
2024-05-08 20:35:11 +0200

    remove: provider config block

--- providers.tf
+++ providers.tf
@@ -1,14 +1,9 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.102.0"
     }
   }
-}
-
-provider "azurerm" {
-  # Configuration options
-  features {}
 }
 
